f6022aa8442c11ef6bc6bb1f3eeeefa2284a3d77
jerry2005hn <jerry2005hn@gmail.com>
2025-06-14 16:56:08 +1200

    last sliding window woohoo

---
+++ sliding-window/567. Permuation in String.py
@@ -0,0 +1,51 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = len(s1) - 1
+        s1Count = Counter(s1)
+        while r < len(s2):
+            s2Count = Counter(s2[l:r+1])
+            if s1Count - s2Count == Counter():
+                return True
+            l+=1
+            r+=1
+        return False
+"""
+Optimal sliding window:
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
+"""
