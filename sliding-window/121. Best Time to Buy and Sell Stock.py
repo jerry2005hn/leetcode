2457d2a126e26b7951ca2c697023adc2d065768d
Jerry Nguyen <jerry2005hn@gmail.com>
2025-12-27 13:21:01 +1300

    feat: reset leetcode

--- sliding-window/121. Best Time to Buy and Sell Stock.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices) -> int:
-        maxP = 0
-        minBuy = prices[0]
-
-        for sell in prices:
-            maxP = max(maxP, sell - minBuy)
-            minBuy = min(minBuy, sell)
-        return maxP
--- stack/22. Generate Parentheses.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def generateParenthesis(self, n: int):
-        
-ans = Solution()
-print(ans.generateParenthesis(n=3))
