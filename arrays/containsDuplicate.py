a1e4fd0c46e32f245e5f357d0c1486a791a7210b
Jerry Nguyen <jerry2005hn@gmail.com>
2025-12-27 13:22:09 +1300

    feat: contains duplicate

---
+++ arrays/containsDuplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+ans = Solution()
+print(ans.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
